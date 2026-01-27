e636dec0c44235b7dddd67966158b1aa508556e1
GeorgeET15 <ironmanrocks353@gmail.com>
2026-01-27 14:33:16 +0530

    removed the website dir

--- weblens-website/src/components/InteractiveBlockDemo.tsx
+++
@@ -1,9 +0,0 @@
-import { FlowEditorDemo } from "@/components/tool/FlowEditorDemo";
-
-export function InteractiveBlockDemo() {
-  return (
-    <div className="w-full">
-       <FlowEditorDemo />
-    </div>
-  );
-}
